7c607b4782703d97bb4babcbd181412fbb86b844
Alexey Timofievich <alex.timofievich@gmail.com>
2020-10-24 22:11:30 +0300

    add get assembly info library

--- Lab_3/lab-3/WpfApp/ApplicationView.cs
+++ Lab_3/lab-3/WpfApp/ApplicationView.cs
@@ -1,10 +1,19 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 namespace WpfApp
 {
     public class ApplicationView : INotifyPropertyChanged
     {
+        public DllInfo SelectedDll;
+
+        public ObservableCollection<DllInfo> DllInfos;
+        
+        public ApplicationView()
+        {
+            DllInfos = new ObservableCollection<DllInfo>();
+        }
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
---
+++ Lab_3/lab-3/WpfApp/DllInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace WpfApp
+{
+    public class DllInfo
+    {
+        public string Path;
+        public string Name;
+
+        public DllInfo(string dllPath, string dllName)
+        {
+            Path = dllPath;
+            Name = dllName;
+        }
+    }
+}
