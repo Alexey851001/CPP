ae8f56f7bc26e337e60056accc7d817a7f7345de
Alexey Timofievich <alex.timofievich@gmail.com>
2020-10-24 19:55:43 +0300

    add wpf application

---
+++ Lab_3/lab-3/WpfApp/ApplicationView.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfApp
+{
+    public class ApplicationView : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
