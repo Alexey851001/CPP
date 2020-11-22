af8e191af86e727d17c1f3c3863d60846008a5b2
Alexey Timofievich <alex.timofievich@gmail.com>
2020-11-22 16:09:39 +0300

    add parallel generate tests

--- Lab_4/lab-4/ConsoleApp/Program.cs
+++ Lab_4/lab-4/ConsoleApp/Program.cs
@@ -34,14 +34,44 @@ namespace ConsoleApp
             MaxDegreeOfParallelism = MaxDegreeOfParallelismLoad
          });
 
+         var generateTestClass = new TransformManyBlock<string, TestClass>(async text =>
+         {
+            Console.WriteLine("Generating test classes...");
          
+            TestClassGenerator classGenerator = new TestClassGenerator();
+
+            return classGenerator.GenerateTestClasses(text);
+         }, new ExecutionDataflowBlockOptions
+         {
+            MaxDegreeOfParallelism = MaxDegreeOfParallelismGenerate
+         });
+
+         var saveTestClassToFile = new ActionBlock<TestClass>(async testClass =>
+         {
+            using (StreamWriter DestinationWriter = File.CreateText(DestPath + testClass.FileName))
+            {
+               Console.WriteLine("Saving '{0}' on disk...", testClass.FileName);
+               await DestinationWriter.WriteAsync(testClass.Source);
+               Console.WriteLine("Saved '{0}'!", testClass.FileName);
+            }
+         }, new ExecutionDataflowBlockOptions
+         {
+            MaxDegreeOfParallelism = MaxDegreeOfParallelismSave
+         });
+
+         var linkOptions = new DataflowLinkOptions { PropagateCompletion = true };
+
+         loadSourceFileToMemory.LinkTo(generateTestClass, linkOptions);
+         generateTestClass.LinkTo(saveTestClassToFile, linkOptions);
+
          foreach (var path in _input)
          {
             loadSourceFileToMemory.Post(path);
          }
 
          loadSourceFileToMemory.Complete();
-         
+
+         saveTestClassToFile.Completion.Wait();
       }
    }
 }
