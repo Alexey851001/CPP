fa3e486364b3725804d1fe452696f8ec015cd941
Alexey Timofievich <alex.timofievich@gmail.com>
2020-11-22 13:54:43 +0300

    Add test generator library

---
+++ Lab_4/lab-4/ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks.Dataflow;
+using TestsClassGenerator;
+
+namespace ConsoleApp
+{
+   class Program
+   {
+      public static void Main(string[] args)
+      {
+         
+      }
+   }
+}
